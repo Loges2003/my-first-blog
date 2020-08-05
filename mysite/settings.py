2abfeedd8a8680018ad42526d8cc5e39f913d687
Loges2003 <52312172+Loges2003@users.noreply.github.com>
2020-08-05 17:10:44 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.db.backends.sqlite3'
+    'django.db.backends.sqlite3',
+    'blog.apps.BlogConfig',
    
 ]
 
